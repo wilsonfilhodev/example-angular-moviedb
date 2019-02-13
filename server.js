5f9b6d732e6b41e6365d62a3f784948c59de0023
Wilson Luiz <wilson.luiz@solutis.com.br>
2019-02-13 10:21:36 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+ 
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/example-angular-moviedb'));
-
+ 
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/example-angular-moviedb'));
+ 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/example-angular-moviedb/index.html'));
+ 
+res.sendFile(path.join(__dirname+'/example-angular-moviedb/index.html'));
 });
-
+ 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
